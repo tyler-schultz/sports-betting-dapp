f19122d17f6db41c04eedbf8402d156cbe616452
patrickkeleher <keleherpatrick1@gmail.com>
2020-04-20 20:11:26 -0600

    Initial files

---
+++ client/src/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#" className="brand-logo center">
+                            Ball'n Chain (Place Holder for Proper Logo)
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Header;
