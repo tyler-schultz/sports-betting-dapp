4a4a37508f6b6a8cacce218534832b9c7bff04ab
patrickkeleher <keleherpatrick1@gmail.com>
2020-04-21 21:21:42 -0600

    Create simple home page and add to betting functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import logo from './Balln Chain Logo.png';
+import Betting from "./pages/Betting.js";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarText
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Ball'n Chain</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="#">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">Create Game</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="./pages/Betting.js">Place Bet</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <NavbarText>Make Your Millions</NavbarText>
+                </Collapse>
+            </Navbar>
+            <img src={logo}/>
+        </div>
+
+
+    );
 }
+
 
 export default App;
