a0d5f8875c84c2ff1a782a523d9e3a807d4ee597
tannermagee <tannermagee33@gmail.com>
2020-05-02 21:08:03 -0600

    Start.

--- client/src/components/GameFrame.js
+++ client/src/components/GameFrame.js
@@ -9,57 +9,34 @@ class GameFrame extends Component {
     }
 
     render() {
-        let todaysGames = [];
-        let historyGames = [];
-
-        for(int i = 0; i < this.props.BC.methods.getGamesToday("TODAY").call(); i++) {
-            let currId = this.props.BC.methods.getGameToday("TODAY", i).call();
-            let currGame = this.props.BC.methods.getGamedData(currId);
-            todaysGames.push({currGame});
-        }
-
-        for(int j = 0; j < this.props.BC.methods.totalUserBets().call(); j++) {
-            let currId = this.props.BC.methods.getGamefromHistory(j).call();
-            let currGame = this.props.BC.methods.getGamedData(currId);
-            historyGames.push({currGame});
-        }
-
         return (
             <div>
                 <h1>Today's Games</h1>
                 <Col>
-                    {todaysGames.map(game)=><Game
-                        id={game.gameId}
-                        image={game.image}
-                        timeStart={game.start}
-                        end={game.end}
-                        date={game.date}
-                        hName={game.hName}
-                        hRecord={game.hRecord}
-                        hBetters={game.hBetters}
-                        aName={game.aName}
-                        aRecord={game.aRecord}
-                        aBetters={game.aBetters}
-                        gameB={game.gameB}/>
-                    }
+                    {this.renderTodayCards()}
+
                 </Col>
                 <h1>Past Games</h1>
                 <Col>
-                    {historyGames.map(game)=><Game
-                        id={game.gameId}
-                        image={game.image}
-                        timeStart={game.start}
-                        end={game.end}
-                        date={game.date}
-                        hName={game.hName}
-                        hRecord={game.hRecord}
-                        hBetters={game.hBetters}
-                        aName={game.aName}
-                        aRecord={game.aRecord}
-                        aBetters={game.aBetters}
-                        gameB={game.gameB}/>
+
                     }
                 </Col>
+            </div>
+        );
+    }
+
+    renderTodayCards(){
+        let todaysGames = [];;
+        for(let i = 0; i < this.props.BC.methods.getGamesToday("05-02-2020").call(); i++) {
+            let currId = this.props.BC.methods.getGameToday("05-02-2020", i).call();
+            let currGame = this.props.BC.methods.getGamedData(currId);
+            todaysGames.push({currGame});
+        }
+
+
+        return(
+            <div>
+                <Game id={}/>
             </div>
         );
     }
