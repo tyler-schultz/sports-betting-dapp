42a755a6cecd72e9cbcfa1f9e11c7964f6d933dd
tyler-schultz <tysch@rams.colostate.edu>
2020-05-02 21:01:00 -0600

    Fixed some mapping errors

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle, CardSubtitle
 } from 'reactstrap';
 
-import Model from "./Model";
 import Bet from "./Bet";
 class Game extends Component {
     constructor(props) {
         super(props);
 
-        let state = {
-            gameID: null,
-            gameDate: new Date()
+        this.state = {
+            betOpen: false,
+            gameData: {
+                gameId: this.props.id,
+                gameImage: this.props.image,
+                timeStart: this.props.start,
+                gameEnd: this.props.end,
+                date: this.props.date,
+                homeTeam: this.props.hName,
+                homeRecord: this.props.hRecord,
+                homeBetters: this.props.hBetters,
+                awayTeam: this.props.aName,
+                awayRecord: this.props.aRecord,
+                awayBetters: this.props.aBetters,
+                gameBalance: this.props.gameB
+            }
         };
+
+        this.toggleBet = this.toggleBet.bind(this);
+        console.log(this.state.gameData);
     }
 
- render() {
+    render() {
+        return (
+            <div>
+            <Card onClick={this.toggleBet} >
+            <CardImg top width="100%" src={this.state.gameData.gameI} alt="Card image"/>
+            <CardBody>
+            <CardTitle>Game ID: {this.state.gameData.gameId}</CardTitle>
+        <CardSubtitle>Date: {this.state.gameData.date}</CardSubtitle>
+        <CardText>{this.state.gameData.homeTeam} VS {this.state.gameData.awayTeam}</CardText>
+        <Bet betOpen={this.state.betOpen} toggleBet={this.toggleBet} gameData={this.state.gameData}/>
+        </CardBody>
+        </Card>
+        </div>);
+    }
 
-     return (
-                <div>
-                    <Card>
-                        <CardImg top width="100%" src="../basketball.jpg" alt="Card image" />
-                        <CardBody>
-                            <CardTitle>Game Number {this.props.gameID}</CardTitle>
-                            <CardSubtitle>{this.props.gameDate}</CardSubtitle>
-                            <CardText>TEST IF CARD RENDERED.</CardText>
-                            <Model>" "</Model>
-                        </CardBody>
-                    </Card>
-                </div>);
-        }}
-
-
+    toggleBet() {
+        this.setState({betOpen: !this.state.betOpen})
+    }
+}
 export default Game;
