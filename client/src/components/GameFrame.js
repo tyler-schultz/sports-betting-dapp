50753d7a1784d464a71dec90642b8055038b4bde
tannermagee <tannermagee33@gmail.com>
2020-05-02 19:31:02 -0600

    Game component.

--- client/src/components/GameFrame.js
+++ client/src/components/GameFrame.js
@@ -17,9 +17,11 @@ class GameFrame extends Component {
         return (
             <div>
                 <h1>Today's Games</h1>
+                <Col>
+                    <Game id="" image="image Pathe" timeStart={0} end={1} date="TODAY" hName="Nugs" hRecord="0-0" hBetters={0} aName="Suns" aRecord="0-0" aBetters={0} gameB={0}/>
+                </Col>
                 <h1>Past Games</h1>
                 <Col>
-                <Game></Game>
                 </Col>
             </div>
         );
